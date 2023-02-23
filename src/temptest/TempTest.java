84eb55ca17ce1b73b8d6e277b21c2c2f727e502d
ongh <ongh@sheridancollege.ca>
2023-02-23 04:33:59 -0500

    Commit 1

---
+++ src/temptest/TempTest.java
@@ -0,0 +1,16 @@
+package temptest;
+
+/**
+ * This class +++Insert Description Here+++
+ * @author Long Ong
+ */
+public class TempTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(1+2);
+    }
+
+}
